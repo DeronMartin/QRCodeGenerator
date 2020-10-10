dfc575ad70c410db4abc58e99a1fa7ec8baf8111
Deron Martin <deronmartin1990@gmail.com>
2020-10-10 13:15:59 +0530

    HTML Complete

--- static/js/script.js
+++ static/js/script.js
@@ -3,4 +3,14 @@ $(document).ready(function(){
     //Adjust Main Section Height
     $("#main").height($("#main").height() - 128);
 
+    //Adjust QR Code Size
+    qr_height = $("#qrcode").height();
+    qr_width = $("#qrcode").width();
+    if (qr_height > qr_width) {
+        $("#qrcode").height(qr_width);
+    }
+    else {
+        $("#qrcode").width(qr_height);
+    }
+
 });
